d1b9afaff838d2817a7b0b8826b212c4855e9f98
Prajwal Chandrashekhar Ulli <ulliprajwaledu@gmail.com>
2025-02-26 15:04:07 +0530

    Create signals.c

---
+++ Simple_Shell/signals.c
@@ -0,0 +1,22 @@
+//SO: https://igupta.in/blog/writing-a-unix-shell-part-3/
+
+#include <stdio.h>
+#include <signal.h>
+
+void SigCall(int num)
+{
+  printf("%d\n", num);
+}
+
+int main(void)
+{
+  signal(SIGINT, SIG_DFL);
+  while (1);
+}
+
+/* SIGNALS */
+// SIGINT (ctrl+C): Signal Interrupt, defaults to terminate process, but can take a disposition (ie func)
+// SIGTSTP (ctrl+Z): Signal Terminal Stop, defaults to stop current process, but can take a disposition (ie func)
+// SIG_IGN: Signal Ignore, passed as a disposition to ignore given signal
+// SIG_DFL: Signal Default, passed as a disposition to let use default functionality of given signal
+// SIGSTOP & SIGKILL: do not take any  disposition other than SIG_DFL, they cannot be caught
